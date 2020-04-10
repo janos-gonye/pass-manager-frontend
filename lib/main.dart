8dded90746eb7c1ad5decd39b4027c708020d596
János Gönye <janos.gonye@gmail.com>
2020-04-10 20:21:06 +0200

    add login page prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pass_manager_frontend/pages/login.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/login',
   routes: {
-    '/login': (context) => HomePage(),
+    '/login': (context) => LoginPage(),
   },
 ));
