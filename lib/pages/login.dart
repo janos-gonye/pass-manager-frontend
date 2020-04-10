6c6164b8e2c69e8252c5db5049d566d7f4ae5f84
János Gönye <janos.gonye@gmail.com>
2020-04-10 22:09:41 +0200

    add symmetric padding to login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,25 +13,31 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.white,
       body: Center(
         child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Johnny\'s\nPassword\nManager',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.grey[800],
-                  fontWeight: FontWeight.w800,
-                  fontSize: 30,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 60,
+              vertical: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Johnny\'s\nPassword\nManager',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w800,
+                    fontSize: 30,
+                  ),
                 ),
-              ),
-              Icon(
-                Icons.vpn_key,
-                color: Colors.grey[800],
-                size: 120,
-              ),
-              LoginForm(),
-            ],
+                Icon(
+                  Icons.vpn_key,
+                  color: Colors.grey[800],
+                  size: 120,
+                ),
+                LoginForm(),
+              ],
+            ),
           ),
         )
       )
