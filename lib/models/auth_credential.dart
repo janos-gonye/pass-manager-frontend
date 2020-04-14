52614c55737031edf2bdeb6146f9c2b5563ab266
János Gönye <janos.gonye@gmail.com>
2020-04-14 22:43:58 +0200

    add model authentication credentials

---
+++ lib/models/auth_credential.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class AuthCredentialModel {
+    String username;
+    String password;
+
+    AuthCredentialModel({@required this.username, @required this.password});
+}
