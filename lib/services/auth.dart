f21c1772d36c08e3a70cec86523207043bbeec27
János Gönye <janos.gonye@gmail.com>
2020-04-15 11:35:17 +0200

    implement 'AuthService.login'

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,11 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:pass_manager_frontend/services/api.dart';
 
+import 'package:pass_manager_frontend/constants.dart' as constants;
+import 'package:pass_manager_frontend/models/auth_credential.dart';
+import 'package:pass_manager_frontend/services/api.dart';
 
 class AuthService extends ApiService {
   static const String _accesTokenName = 'accessToken';
   static const String _refreshTokenName = 'refreshToken';
+
+  static Future<bool> login(AuthCredential authCredential) async {
+    Response response = await post(
+      constants.PATH_OBTAIN_TOKENS,
+      body: authCredential.toJson());
+    if (response.statusCode == 401) {
+      return false;
+    }
+    final Map <String, String> tokens = jsonDecode(response.body);
+    accessToken = tokens["access"];
+    refreshToken = tokens["refresh"];
+    return true;
+  }
 
   static void _setToken({@required String key, @required String value}) {
     final FlutterSecureStorage secureStorage = FlutterSecureStorage();
