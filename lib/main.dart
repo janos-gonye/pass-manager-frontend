a29404b09e627f95213a00398c34c51895af39dc
János Gönye <janos.gonye@gmail.com>
2020-04-20 22:38:20 +0200

    register 'ProfilesPage' in the routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:pass_manager_frontend/pages/login.dart';
 import 'package:pass_manager_frontend/constants.dart' as constants;
 import 'package:pass_manager_frontend/pages/settings.dart';
+import 'package:pass_manager_frontend/pages/profiles.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: constants.ROUTE_LOGIN,
   routes: {
     constants.ROUTE_LOGIN: (context) => LoginPage(),
     constants.ROUTE_SETTINGS: (context) => SettingsPage(),
+    constants.ROUTE_PROFILES: (context) => ProfilesPage(),
   },
 ));
