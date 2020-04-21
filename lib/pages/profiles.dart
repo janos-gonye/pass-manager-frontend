7881dc92740974f965dfe437d54e9f23d36a9e2e
János Gönye <janos.gonye@gmail.com>
2020-04-21 21:25:16 +0200

    show login success message after building up 'ProfilesPage'

--- lib/pages/profiles.dart
+++ lib/pages/profiles.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class ProfilesPage extends StatelessWidget {
+class ProfilesPage extends StatefulWidget {
+  @override
+  _ProfilesPageState createState() => _ProfilesPageState();
+}
+
+class _ProfilesPageState extends State<ProfilesPage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  Map<String, String> _pageArgs;
+
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+        (_) => _scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text(_pageArgs["message"]),
+              duration: Duration(seconds: 2),
+            )));
+  }
 
   @override
   Widget build(BuildContext context) {
+    _pageArgs = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
