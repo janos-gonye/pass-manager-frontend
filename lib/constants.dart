2cd3af438ad8ebb6c5d4147fb6a15a76cee5a8fc
János Gönye <janos.gonye@gmail.com>
2020-04-23 22:30:35 +0200

    add 'ROUTE_MASTER_PASS' to 'constants'

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 const String ROUTE_LOGIN = '/login';
 const String ROUTE_SETTINGS = '/settings';
 const String ROUTE_PROFILES = '/profiles';
+const String ROUTE_MASTER_PASS = '/master-password';
 
 const String PATH_OBTAIN_TOKENS = '/token/';
 const String PATH_PROFILES = '/profiles/';
