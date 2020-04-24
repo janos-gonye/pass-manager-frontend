ca484aa77daebebe379b1432da499b089cc02f44
János Gönye <janos.gonye@gmail.com>
2020-04-24 22:15:27 +0200

    implement basic UI of the 'ProfilePage'

--- lib/pages/profiles.dart
+++ lib/pages/profiles.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pass_manager_frontend/services/profile.dart';
+import 'package:pass_manager_frontend/models/profile.dart';
 
 class ProfilesPage extends StatefulWidget {
   @override
@@ -8,6 +10,8 @@ class ProfilesPage extends StatefulWidget {
 class _ProfilesPageState extends State<ProfilesPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   Map<String, String> _pageArgs;
+  final ProfileService _profileService = ProfileService();
+  Future<List<Profile>> _profiles;
 
   void initState() {
     super.initState();
@@ -16,6 +20,8 @@ class _ProfilesPageState extends State<ProfilesPage> {
               content: Text(_pageArgs["message"]),
               duration: Duration(seconds: 2),
             )));
+    // TODO: Handle when 'profiles' contains no empty list
+    _profiles = _profileService.getProfiles();
   }
 
   @override
@@ -25,7 +31,34 @@ class _ProfilesPageState extends State<ProfilesPage> {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Colors.white,
-      body: Center(child: Text("Profiles page")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(60, 130, 60, 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text('Your\nAccounts',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.w800,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        )
+      )
     );
   }
 }
