183ff61e68d969a6ac95431d3a367e175e92cdd8
János Gönye <janos.gonye@gmail.com>
2020-04-28 22:03:02 +0200

    add 'isEmpty' method to 'Profile' model

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -12,4 +12,16 @@ class Profile {
     this.notes = "",
     this.url = "",
   });
+
+  bool isEmpty() {
+    if (this.title.isEmpty &&
+        this.username.isEmpty &&
+        this.password.isEmpty &&
+        this.notes.isEmpty &&
+        this.url.isEmpty
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
