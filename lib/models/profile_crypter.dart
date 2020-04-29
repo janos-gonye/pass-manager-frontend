ea72b6e55632826c3c13dfc47910efe9aa7b581f
János Gönye <janos.gonye@gmail.com>
2020-04-29 23:28:13 +0200

    add docstring to 'ProfileCrypter'

--- lib/models/profile_crypter.dart
+++ lib/models/profile_crypter.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 
+/// This model represents an object that contains
+/// all the information needed to en/decrypt the user's profiles.
 class ProfileCrypter {
   String masterPassword;
 
