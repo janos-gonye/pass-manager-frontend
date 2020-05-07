46bdd28111dd2dc7b0e1553950d33ce0f0cce42f
János Gönye <janos.gonye@gmail.com>
2020-05-08 00:06:46 +0200

    add blocprovider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pass_manager_frontend/blocs/profile/profile_bloc.dart';
 import 'package:pass_manager_frontend/pages/login.dart';
 import 'package:pass_manager_frontend/constants.dart' as constants;
 import 'package:pass_manager_frontend/pages/master_pass.dart';
 import 'package:pass_manager_frontend/pages/settings.dart';
 import 'package:pass_manager_frontend/pages/profiles.dart';
+import 'package:pass_manager_frontend/services/profile.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: constants.ROUTE_LOGIN,
@@ -11,6 +14,9 @@ void main() => runApp(MaterialApp(
     constants.ROUTE_LOGIN: (context) => LoginPage(),
     constants.ROUTE_SETTINGS: (context) => SettingsPage(),
     constants.ROUTE_MASTER_PASS: (context) => MasterPassPage(),
-    constants.ROUTE_PROFILES: (context) => ProfilesPage(),
+    constants.ROUTE_PROFILES: (context) => BlocProvider(
+      create: (context) => ProfileBloc(ProfileService()),
+      child: ProfilesPage(),
+    ),
   },
 ));
