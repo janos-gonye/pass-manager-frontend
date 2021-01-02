3af76119e268201b17d463d8d68c517383fad8a3
János Gönye <janos.gonye@gmail.com>
2021-01-03 00:21:27 +0100

    add edit/delete events to 'cubit/profile_state'

--- lib/cubit/profile_state.dart
+++ lib/cubit/profile_state.dart
@@ -24,17 +24,46 @@ class ProfileLoaded extends ProfileState {
   List<Object> get props => [profiles];
 }
 
-// Saving/editing
-class ProfileSaving extends ProfileState {
-  const ProfileSaving();
+// Create
+class ProfileAddding extends ProfileState {
+  const ProfileAddding();
   @override
   List<Object> get props => [];
 }
 
-class ProfileSaved extends ProfileState {
-  const ProfileSaved();
+class ProfileAdded extends ProfileState {
+  final List<Profile> profiles;
+  const ProfileAdded(this.profiles);
+  @override
+  List<Object> get props => [profiles];
+}
+
+// Edit
+class ProfileEditing extends ProfileState {
+  const ProfileEditing();
   @override
   List<Object> get props => [];
+}
+
+class ProfileEdited extends ProfileState {
+  final List<Profile> profiles;
+  const ProfileEdited(this.profiles);
+  @override
+  List<Object> get props => [profiles];
+}
+
+// Delete
+class ProfileDeleting extends ProfileState {
+  const ProfileDeleting();
+  @override
+  List<Object> get props => [];
+}
+
+class ProfileDeleted extends ProfileState {
+  final List<Profile> profiles;
+  const ProfileDeleted(this.profiles);
+  @override
+  List<Object> get props => [profiles];
 }
 
 // Error
