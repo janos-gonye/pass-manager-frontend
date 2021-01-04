f4bd7be0a3e489bbef9118bf7ace3ea8ba7cd7f3
János Gönye <janos.gonye@gmail.com>
2021-01-04 02:11:18 +0100

    implement logout button component

---
+++ lib/components/buttons/logout.dart
@@ -0,0 +1,25 @@
+import 'dart:math' as math;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pass_manager_frontend/constants.dart' as constants;
+import 'package:flutter/material.dart';
+import 'package:pass_manager_frontend/services/auth.dart';
+
+class LogoutButton extends StatelessWidget {
+  LogoutButton({Key key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        AuthService.logout();
+        Navigator.pushNamedAndRemoveUntil(
+            context, constants.ROUTE_LOGIN, (context) => false);
+      },
+      child: Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.rotationY(math.pi),
+        child: Icon(FontAwesomeIcons.signOutAlt),
+      ),
+      backgroundColor: Colors.grey[800],
+    );
+  }
+}
