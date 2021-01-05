719bd0060c4d2f710392551f2954be0a7ba73108
János Gönye <janos.gonye@gmail.com>
2021-01-05 10:50:43 +0100

    extract the page arguments from login page in the 'initState' method of the '_MasterPassPageState' rather than in the 'build' fuction

--- lib/pages/master_pass.dart
+++ lib/pages/master_pass.dart
@@ -18,17 +18,17 @@ class _MasterPassPageState extends State<MasterPassPage> {
 
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback(
-        (_) => _scaffoldKey.currentState.showSnackBar(SnackBar(
-              content: Text(_pageArgs["message"]),
-              duration: Duration(seconds: 2),
-            )));
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _pageArgs = ModalRoute.of(context).settings.arguments;
+      if (_pageArgs.containsKey("message"))
+        _scaffoldKey.currentState.showSnackBar(SnackBar(
+            content: Text(_pageArgs["message"]),
+            duration: Duration(seconds: 2)));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _pageArgs = ModalRoute.of(context).settings.arguments;
-
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Colors.white,
