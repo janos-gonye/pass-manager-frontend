8f7ada81621dec52a39ec3812e0e55bcbbe13946
János Gönye <janos.gonye@gmail.com>
2021-03-07 13:16:51 +0100

    remove 'lib/theme_data.dart'

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 const String ROUTE_LOGIN = '/login';
 const String ROUTE_SETTINGS = '/settings';
 const String ROUTE_PROFILES = '/profiles';
@@ -9,11 +7,5 @@ const String PATH_OBTAIN_TOKENS = '/token/';
 const String PATH_REFRESH_TOKEN = '/token/refresh';
 const String PATH_VERIFY_TOKEN = '/token/verify';
 const String PATH_PROFILES = '/profiles/';
-
-// Colors
-const Color darkThemePrimary = Color.fromRGBO(240, 84, 84, 1);
-const Color darkThemeSecondary = Color.fromRGBO(221, 221, 221, 1);
-const Color darkThemeForeground = Color.fromRGBO(26, 26, 26, 1);
-const Color darkThemeBackground = Color.fromRGBO(18, 18, 18, 1);
 
 const Duration requestTimeout = Duration(seconds: 8);
